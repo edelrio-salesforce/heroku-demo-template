5be7c8fee6695cf67642733c69ef3e5ecfa7295c
Diego Prestes <diego.prestes@nodo.cc>
2018-10-26 07:50:31 -0300

    added default structure for the template

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
